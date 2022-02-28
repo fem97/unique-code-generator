374296e0562e98f73404e33385d002928fad8239
Shahriar <fem.nwu@gmail.com>
2022-02-28 12:20:56 +0600

    upda ted

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const userHandler = require("./routeHandler/userHandler");
+
+app.use("/user", userHandler);
 
 app.listen(3333, () => {
   console.log("Server running on port: 3333");
--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -1,2 +1,12 @@
 const express = require("express");
-const app = express();
+const router = express.Router();
+
+router.get("/login", async (req, res) => {
+  res.send("Hello");
+});
+
+router.get("/register", async (req, res) => {
+  res.send("Welcome");
+});
+
+module.exports = router;
